86162ce670720335c80bd7eb57d17d11321d6262
Giannina Miñano <g.isabella.md@gmail.com>
2024-06-22 20:02:00 -0300

    Update: Add Tailwind using the CLI and start styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,28 +3,23 @@ module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
     colors: {
-      'blue': '#1fb6ff',
-      'purple': '#7e5bef',
-      'pink': '#ff49db',
-      'orange': '#ff7849',
-      'green': '#13ce66',
-      'yellow': '#ffc82c',
-      'gray-dark': '#273444',
-      'gray': '#8492a6',
-      'gray-light': '#d3dce6',
+      'dark-charcoal': '#312E2C',
+      'dark-raspberry': '#7A284E',
+      'brandy-red': '#854632',
+      'wenge-brown': '#5F564D',
+      'eggshell': '#F3E5D7',
+      'white-coffee': '#E3DDD7',
+      'snow': '#FFF7FB',
+      'white': '#FFFFFF',
+      
     },
     fontFamily: {
       sans: ['Graphik', 'sans-serif'],
       serif: ['Merriweather', 'serif'],
+      
     },
     extend: {
-      spacing: {
-        '8xl': '96rem',
-        '9xl': '128rem',
-      },
-      borderRadius: {
-        '4xl': '2rem',
-      }
-    }
+
+    },
   },
 }
